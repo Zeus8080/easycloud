16331acff5af118ac4a6ab94365b18197b503de3
zhuchong <1043199504@qq.com>
2018-11-04 21:13:28 +0800

    创建springcloud项目

---
+++ src/main/java/com/zhu/easycloud/EasycloudApplication.java
@@ -0,0 +1,12 @@
+package com.zhu.easycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasycloudApplication.class, args);
+    }
+}
